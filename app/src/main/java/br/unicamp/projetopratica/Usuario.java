e372e3d828bb6ba05e71be03542d2cb10dac33c1
ventoturmeses <cc20124@g.unicamp.br>
2021-09-22 18:35:43 -0300

    classes evento e usuario

---
+++ app/src/main/java/br/unicamp/projetopratica/Usuario.java
@@ -0,0 +1,53 @@
+package br.unicamp.projetopratica;
+
+public class Usuario {
+    private String username, nome, nascimento, email, senha;
+
+    public Usuario(String username, String nome, String nascimento, String email, String senha) {
+        this.username = username;
+        this.nome = nome;
+        this.nascimento = nascimento;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
