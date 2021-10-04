198999380e5d393d1e3ea9c6f55368836c2d505c
artur <75857033+ventoturmeses@users.noreply.github.com>
2021-10-04 11:41:24 -0300

    todos os metodos classe service

--- AppTriunfalEvent/app/src/main/java/br/unicamp/apptriunfalevent/APIconfig/Service.java
+++ AppTriunfalEvent/app/src/main/java/br/unicamp/apptriunfalevent/APIconfig/Service.java
@@ -12,32 +12,104 @@ import retrofit2.http.Path;
 
 public interface Service {
 
-
-    /******************* CRUD Usuarios ****************************/
+   /******************* CRUD USUARIOS *******************/
     @GET("/usuario")
     Call<List<Usuario>> getUsuarios();
 
-    @GET("api/usuario/{username}/")
+    @GET("api/usuario/{username}")
     Call<Usuario> getUsuario(@Path("username") String username);
 
     @POST("/api/usuario/")
     Call<Usuario> postUsuario(@Body Usuario usuario);
 
     @PUT("/api/usuario/{username}")
-    Call<Usuario> alterarUser(@Path("username") String username, @Body Usuario usuario);
+    Call<Usuario> putUsuario(@Path("username") String username, @Body Usuario usuario);
 
-/*
-    @GET("/api/dog/get")
-    Call<List<Dog>> getDog();
-
-    @DELETE("/api/dog/delete/{id}")
-    Call<Dog> excluirDog(@Path("id") String id);
-
-    @POST("/api/dog/post")
-    Call<Dog> incluirDog(@Body Dog dog);
+    @DELETE("/api/usuario/{username}")
+    Call<Usuario> deleteUsuario(@Path("username") String username);
 
 
-*/
+    /******************* CRUD EVENTOS *******************/
+    @GET("/evento")
+    Call<List<Evento>> getEventos();
+
+    @GET("api/evento/{id}")
+    Call<Evento> getEvento(@Path("id") String id);
+
+    @POST("/api/evento/")
+    Call<Usuario> postEvento(@Body Evento event);
+
+    @PUT("/api/evento/{id}")
+    Call<Usuario> putEvento(@Path("id") String id, @Body Evento event);
+
+    @DELETE("/api/evento/{id}")
+    Call<Usuario> deleteEvento(@Path("id") String id);
 
 
+    /******************* CRUD CONVIDADOS *******************/
+    @GET("/convidado")
+    Call<List<Convidado>> getConvidados();
+
+    @GET("api/convidado/{idConvidado}")
+    Call<Convidado> getConvidado(@Path("idConvidado") String id);
+
+    @POST("/api/convidado/")
+    Call<Convidado> postConvidado(@Body Convidado convidado);
+
+    @PUT("/api/convidado/{idConvidado}")
+    Call<Convidado> putConvidado(@Path("idConvidado") String id, @Body Convidado convidado);
+
+    @DELETE("/api/convidado/{idConvidado}")
+    Call<Convidado> deleteConvidado(@Path("idConvidado") String id);
+
+
+    /******************* CRUD LEMBRETES *******************/
+    @GET("/lembrete")
+    Call<List<Lembrete>> getLembretes();
+
+    @GET("api/convidado/{id}")
+    Call<Lembrete> getLembrete(@Path("id") String id);
+
+    @POST("/api/convidado/")
+    Call<Lembrete> postLembrete(@Body Lembrete lembrete);
+
+    @PUT("/api/convidado/{id}")
+    Call<Lembrete> putLembrete(@Path("id") String id, @Body Lembrete lembrete);
+
+    @DELETE("/api/convidado/{id}")
+    Call<Lembrete> deleteLembrete(@Path("id") String id);
+
+
+    /******************* CRUD DICAS *******************/
+    @GET("/dica")
+    Call<List<Dica>> getDicas();
+
+    @GET("api/dica/{id}")
+    Call<Dica> getDica(@Path("id") String id);
+
+    @POST("/api/dica/")
+    Call<Dica> postDica(@Body Dica dica);
+
+    @PUT("/api/dica/{id}")
+    Call<Dica> putDica(@Path("id") String id, @Body Dica dica);
+
+    @DELETE("/api/dica/{id}")
+    Call<Dica> deleteDica(@Path("id") String id);
+
+
+    /******************* CRUD FERIADOS *******************/
+    @GET("/feriado")
+    Call<List<Feriado>> getFeriados();
+
+    @GET("api/feriado/{id}")
+    Call<Dica> getFeriado(@Path("id") String id);
+
+    @POST("/api/feriado/")
+    Call<Dica> postFeriado(@Body Dica feriado);
+
+    @PUT("/api/feriado/{id}")
+    Call<Dica> putFeriado(@Path("id") String id, @Body Dica feriado);
+
+    @DELETE("/api/feriado/{id}")
+    Call<Dica> deleteFeriado(@Path("id") String id);
 }
