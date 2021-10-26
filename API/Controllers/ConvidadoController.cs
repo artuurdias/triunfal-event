ee236c5654f38fbf7bb4981355025091053e6546
artur <75857033+ventoturmeses@users.noreply.github.com>
2021-10-26 10:55:45 -0300

    exclusão identities

--- API/Controllers/ConvidadoController.cs
+++ API/Controllers/ConvidadoController.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
-using static System.Console;
 using API.Models;
 using API.Data;
 
@@ -21,68 +20,22 @@ namespace API.Controllers
         }
 
         [HttpGet]
-        public ActionResult<List<Convidado>>  GetAll()
+        public ActionResult<List<Convidado>> GetAll()
         {
             return _context.Convidado.ToList();
         }
 
-        [HttpGet("{idEvento}")]
-        public ActionResult<List<Convidado>> GetById(string idEvento)
+        [HttpGet("{nomeUsuario}/{idEvento}/")]
+        public ActionResult<Convidado> GetByIdUsername(string nomeUsuario, string idEvento)
         {
             try
             {
-                var convidados = _context.Convidado.ToArray();
-                Convidado[] lista = new Convidado[convidados.Length];
-                WriteLine($"Length: {convidados.Length}");
+                var convidado = _context.Convidado.Find(nomeUsuario, idEvento.ToUpper());
 
-                int cont=0;
+                if (convidado == null)
+                    return NotFound();
 
-                for (int i=0; i < convidados.Length; i++) {
-                    if (convidados[i].idEvento == idEvento) {
-                        lista[i] = convidados[i];
-                        cont++;
-                    }
-                }
-                
-                WriteLine($"Cont: {cont}");             
-
-                Convidado[] todos = new Convidado[cont];
-
-                for (int i=0; i < cont; i++) {
-                    todos[i] = lista[i];
-                }
-
-                return todos.ToList();
-            }
-            catch
-            {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados. ");
-            }
-        }
-
-        [HttpGet("{idEvento}/{username}")]
-        public ActionResult<Convidado> GetByIdUsername(string idEvento, string username)
-        {
-            try
-            {
-                /*var convidado = _context.Convidado.Find(idEvento, username);
-                WriteLine($"Convidado: {convidado.nomeUsuario} --- Evento: {convidado.idEvento}");
-                return convidado;      */
-
-                var convidados = _context.Convidado.ToArray();
-                Convidado[] lista = new Convidado[convidados.Length];
-                WriteLine($"Length: {convidados.Length}");
-
-                
-                for (int i=0; i < convidados.Length; i++) {
-                    if (convidados[i].idEvento == idEvento && convidados[i].nomeUsuario == username) {     
-                        WriteLine($"Convidado: {convidados[i].nomeUsuario} --- Evento: {convidados[i].idEvento}");
-                        return convidados[i];      
-                    }
-                }    
-                
-                return _context.Convidado.Find(-1);
-                    
+                return convidado;
             }
             catch
             {
@@ -99,7 +52,7 @@ namespace API.Controllers
                 _context.Convidado.Add(convidado);
 
                 if (await _context.SaveChangesAsync() == 1)
-                    return Created($"/api/convidado/{convidado.idEvento}/{convidado.nomeUsuario}", convidado);
+                    return Created($"/api/convidado/{convidado.nomeUsuario}/{convidado.idEvento}", convidado);
             }
             catch
             {
@@ -108,27 +61,19 @@ namespace API.Controllers
             return BadRequest();
         }
 
-        [HttpDelete("{idEvento}/{username}")]
-        public async Task<ActionResult> Delete(string idEvento, string username)
+        [HttpDelete("{nomeUsuario}/{idEvento}")]
+        public async Task<ActionResult> Delete(string nomeUsuario, string idEvento)
         {
             try
-            { 
+            {
+                var convidado = _context.Convidado.Find(nomeUsuario, idEvento);
 
-                
-                var convidados = _context.Convidado.ToArray();
-                Convidado[] lista = new Convidado[convidados.Length];
-                WriteLine($"Length: {convidados.Length}");
+                if (convidado == null)
+                    return NotFound();
 
-                
-                for (int i=0; i < convidados.Length; i++) {
-                    if (convidados[i].idEvento == idEvento && convidados[i].nomeUsuario == username) {     
-                        WriteLine($"Convidado: {convidados[i].nomeUsuario} --- Evento: {convidados[i].idEvento}");
-                        _context.Remove(convidados[i]);
-                              await _context.SaveChangesAsync();
-                        return NoContent();
-                    }
-                }    
-                
+                _context.Remove(convidado);
+                await _context.SaveChangesAsync();
+
                 return NoContent();
             }
             catch
