2c0e2e14b2aec81771dfb2bd3ab39edbc7ba0484
artur <75857033+ventoturmeses@users.noreply.github.com>
2021-10-26 10:51:03 -0300

    exclusão identities

--- API/Data/Context.cs
+++ API/Data/Context.cs
@@ -8,6 +8,22 @@ namespace API.Data
         public Context(DbContextOptions<Context> options): base (options)
         { }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Convidado>().HasKey(c => new {
+                c.nomeUsuario, c.idEvento
+            });
+            modelBuilder.Entity<Convite>().HasKey(c => new {
+                c.nomeUsuario, c.idEvento 
+            });
+            modelBuilder.Entity<Dica>().HasKey(d => new {
+                d.tipoEvento, d.conteudo
+            });
+            modelBuilder.Entity<Lembrete>().HasKey(l => new {
+                l.usuario, l.data
+            });
+        }
+
         public DbSet<Usuario> Usuario { get; set; }
         public DbSet<Evento> Evento { get; set; }
         public DbSet<Convidado> Convidado { get; set; }
