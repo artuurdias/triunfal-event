141d7ffd2c0c422f92ba8a786ff000289299091f
artur <75857033+ventoturmeses@users.noreply.github.com>
2021-10-26 10:52:26 -0300

    exclusão identities

--- API/Controllers/ConviteController .cs
+++ API/Controllers/ConviteController .cs
@@ -25,28 +25,33 @@ namespace API.Controllers
             return _context.Convite.ToList();
         }
 
-        [HttpGet("{id}")]
-        public ActionResult<Convite> GetById(int id)
+        [HttpGet("{nomeUsuario}/{idEvento}/")]
+        public ActionResult<Convite> GetByIdUsername(string nomeUsuario, string idEvento)
         {
             try
             {
-                return _context.Convite.Find(id);
+                var convite = _context.Convite.Find(nomeUsuario, idEvento.ToUpper());
+
+                if (convite == null)
+                    return NotFound();
+
+                return convite;
             }
             catch
             {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados. ");
             }
         }
 
         [HttpPost]
-        public async Task<ActionResult> PostEvent(Convite evento)
+        public async Task<ActionResult> PostEvent(Convite convite)
         {
             try
             {
-                _context.Convite.Add(evento);
+                _context.Convite.Add(convite);
 
                 if (await _context.SaveChangesAsync() == 1)
-                    return Created($"/api/convite/{evento.id}", evento);
+                    return Created($"/api/convite/{convite.nomeUsuario}/{convite.idEvento}", convite);
             }
             catch
             {
@@ -55,44 +60,19 @@ namespace API.Controllers
             return BadRequest();
         }
 
-        [HttpDelete("{id}")]
-        public async Task<ActionResult> Delete(int id)
+        [HttpDelete("{nomeUsuario}/{idEvento}")]
+        public async Task<ActionResult> Delete(string nomeUsuario, string idEvento)
         {
             try
             {
-                var evento = await _context.Convite.FindAsync(id);
+                var convite = await _context.Convite.FindAsync(nomeUsuario, idEvento);
 
-                if (evento == null)
+                if (convite == null)
                     return NotFound();
 
-                _context.Remove(evento);
+                _context.Remove(convite);
                 await _context.SaveChangesAsync();
 
-                return NoContent();
-            }
-            catch
-            {
-                return this.StatusCode(StatusCodes.Status500InternalServerError, "Falha no acesso ao banco de dados.");
-            }
-        }
-
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, Convite evento)
-        {
-            try
-            {
-                var result = await _context.Convite.FindAsync(id);
-                if (id != result.id)
-                {
-                    return BadRequest();
-                }
-                result.id = evento.id;
-                result.data = evento.data;
-                result.mensagem = evento.mensagem;
-                result.idEvento = evento.idEvento;
-                result.nomeUsuario = evento.nomeUsuario;
-
-                await _context.SaveChangesAsync();
                 return NoContent();
             }
             catch
